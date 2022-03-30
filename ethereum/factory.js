57761fa340b17a860e7115050050237b7ba943cb
ukabyl <umkabylbekov@gmail.com>
2022-03-30 22:17:52 +0500

    changed address of campaign factory contract

--- ethereum/factory.js
+++ ethereum/factory.js
@@ -1,6 +1,6 @@
 import web3 from './web3';
 import compiledFactory from './build/CampaignFactory.json';
 
-const instance = new web3.eth.Contract(compiledFactory.abi, '0x3bc6f0F4C2972d25e728880bd91fd008a7b1dA46');
+const instance = new web3.eth.Contract(compiledFactory.abi, '0x9BDc6Eb714f5dbC6376F30E82D0c719c32C5e3bc');
 
 export default instance;
